1fe3ead48775afde0021243f0baf042f9551379a
BorderlessDev <borderlesspc1@gmail.com>
2023-09-28 17:48:08 -0300

    aa

--- server.js
+++ server.js
@@ -2,19 +2,17 @@ import express from 'express';
 import { InMemorySigner } from '@taquito/signer';
 import { TezosToolkit } from '@taquito/taquito';
 
-const { DAppClient } = pkg;
 // Configurações
 const app = express();
 const port = process.env.PORT || 3000; // Use a porta definida no ambiente ou 3000
 
+const private_key = "edskS55SstJHyYXDJY2qZ8TT1s35ZqGtYqVPPyKiu5rkUhP6nfkWfWiFNhaDjcnY1uwYzho4yZAMAygPwQuawutZDtRrrcgWUZ"
+const Tezos = new TezosToolkit("https://ghostnet.ecadinfra.com")
+Tezos.setProvider({ signer: await InMemorySigner.fromSecretKey(private_key) });
+const contrato = "KT1EFLadgpu6EjSh4qrQP1BsxGyjP3cHh6cu"
+
 app.get('/auction', async (req, res) => {
   try {
-    // definindo as variaveis
-    const private_key = "edskS55SstJHyYXDJY2qZ8TT1s35ZqGtYqVPPyKiu5rkUhP6nfkWfWiFNhaDjcnY1uwYzho4yZAMAygPwQuawutZDtRrrcgWUZ"
-    const Tezos = new TezosToolkit("https://ghostnet.ecadinfra.com")
-    Tezos.setProvider({ signer: await InMemorySigner.fromSecretKey(private_key) });
-    const contrato = "KT1EFLadgpu6EjSh4qrQP1BsxGyjP3cHh6cu"
-    // Função de aceitar
     const contract = Tezos.wallet.at(contrato)
     const op = await contract.methods.finalize_auction(1, 5, 1);
     
