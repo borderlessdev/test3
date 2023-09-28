4a333f2d1662c19a03b9560fd4b3bab0694286ec
BorderlessDev <borderlesspc1@gmail.com>
2023-09-28 17:30:56 -0300

    testando auction

--- server.js
+++ server.js
@@ -1,7 +1,5 @@
 import express from 'express';
-import { BeaconWallet } from '@taquito/beacon-wallet';
-// import { DAppClient } from '@temple-wallet/dapp';
-import pkg from '@temple-wallet/dapp';
+import { InMemorySigner } from '@taquito/signer';
 import { TezosToolkit } from '@taquito/taquito';
 
 const { DAppClient } = pkg;
@@ -9,59 +7,23 @@ const { DAppClient } = pkg;
 const app = express();
 const port = process.env.PORT || 3000; // Use a porta definida no ambiente ou 3000
 
-// Inicializando o BeaconWallet
-const wallet = new BeaconWallet({
-  name: 'Borderless ',
-  preferredNetwork: 'mainnet', // or 'delphinet' for Tezos testnet
-  disableDefaultEvents: true,
-  eventHandlers: {
-    PERMISSION_REQUEST_SUCCESS: {
-      handler: async (data) => {
-        console.log('Permission request successful:', data);
-      },
-    },
-  },
-});
-
-// Inicializando o DAppClient e TezosToolkit
-const client = new DAppClient({ name: 'Borderless' });
-const tezos = new TezosToolkit(client);
-
-// Endpoint para solicitar autorização de carteira
-app.get('/authorize', async (req, res) => {
+app.get('/auction', async (req, res) => {
   try {
-    // Inicia o processo de autorização com a carteira Beacon
-    await wallet.requestPermissions({
-      network: {
-        type: 'mainnet', // or 'delphinet'
-      },
+    // definindo as variaveis
+    const private_key = "edskS55SstJHyYXDJY2qZ8TT1s35ZqGtYqVPPyKiu5rkUhP6nfkWfWiFNhaDjcnY1uwYzho4yZAMAygPwQuawutZDtRrrcgWUZ"
+    const Tezos = new TezosToolkit("https://ghostnet.ecadinfra.com")
+    Tezos.setProvider({ signer: await InMemorySigner.fromSecretKey(private_key) });
+    const contrato = "KT1EFLadgpu6EjSh4qrQP1BsxGyjP3cHh6cu"
+    // Função de aceitar
+    const contract = Tezos.wallet.at(contrato)
+    const op = await contract.methods.finalize_auction(1, 5, 1);
+    
+    res.status(200).json({ message: 'Transação aceita' });
+      } catch (error) {
+        console.error('Error', error)
+        res.status(500).json({ error: 'An error occurred'})
+      }
     });
-
-    res.status(200).json({ message: 'Wallet authorized successfully' });
-  } catch (error) {
-    console.error('Error:', error);
-    res.status(500).json({ error: 'An error occurred' });
-  }
-});
-
-// Endpoint para receber os dados do Bubble.io e criar a NFT
-app.post('/createNFT', async (req, res) => {
-  try {
-    const { nftName, nftDescription, nftImage } = req.body;
-
-    // Enviar transação para o contrato inteligente
-    const contract = await tezos.wallet.at('KT1FVfi5SFiWay5eqSX7Pr37ztP23oTnRuHi');
-    const operation = await contract.methods.createNFT(nftName, nftDescription, nftImage).send();
-
-    // Aguardar confirmação da transação
-    await operation.confirmation();
-
-    res.status(200).json({ message: 'NFT created successfully' });
-  } catch (error) {
-    console.error('Error:', error);
-    res.status(500).json({ error: 'An error occurred' });
-  }
-});
 
 // Iniciando o servidor
 app.listen(port, () => {
