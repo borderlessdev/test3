6d21e23665532edb9818f5e6d07e7fde8a166acd
BorderlessDev <borderlesspc1@gmail.com>
2023-09-29 15:13:11 -0300

    d

--- server.js
+++ server.js
@@ -13,17 +13,15 @@ const privateKey = new aws.S3({
 })
 const contrato = "KT1EFLadgpu6EjSh4qrQP1BsxGyjP3cHh6cu"
 const Tezos = new TezosToolkit("https://ghostnet.ecadinfra.com")
-//Tezos.setProvider({ signer: await InMemorySigner.fromSecretKey(privateKey.privateKey) });
+Tezos.setProvider({ signer: await InMemorySigner.fromSecretKey(JSON.stringify(privateKey.config.privateKey)) });
 
 
 app.post('/auction', async (req, res) => {
   try {
         const { auction_id, edition, token_id } = req.body
-        console.log(JSON.stringify(privateKey))
-        console.log(JSON.stringify(privateKey.config.privateKey))
         const contract = await Tezos.wallet.at(contrato)
-        //const op = await contract.methods.finalize_auction(auction_id, edition, token_id).send();
-        res.status(200).json({ message: `Transação aceita  ${op.opHash}` });
+        const op = await contract.methods.finalize_auction(auction_id, edition, token_id).send();
+        res.status(200).json({ message: `Transação aceita, op Hash:  ${op.opHash}` });
           } catch (error) {
             console.error('Error', error)
             res.status(500).json({ error: 'An error occurred'})
