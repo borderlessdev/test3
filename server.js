a370887aeea569ef7c58a7d2cd2b26f7246db150
BorderlessDev <borderlesspc1@gmail.com>
2023-09-29 14:27:30 -0300

    d

--- server.js
+++ server.js
@@ -1,25 +1,29 @@
 import express from 'express';
 import { InMemorySigner } from '@taquito/signer';
 import { TezosToolkit } from '@taquito/taquito';
+import { aws } from 'aws-sdk'
 
 // Configurações
 const app = express();
-const port = process.env.PORT || 3000; // Use a porta definida no ambiente ou 3000
 app.use(express.json())
+const port = process.env.PORT || 3000; // Use a porta definida no ambiente ou 3000
 
-const private_key = "edskS55SstJHyYXDJY2qZ8TT1s35ZqGtYqVPPyKiu5rkUhP6nfkWfWiFNhaDjcnY1uwYzho4yZAMAygPwQuawutZDtRrrcgWUZ"
+const privateKey = new aws.S3({
+  privateKey: process.end.privateKey
+})
 const contrato = "KT1EFLadgpu6EjSh4qrQP1BsxGyjP3cHh6cu"
 const Tezos = new TezosToolkit("https://ghostnet.ecadinfra.com")
-Tezos.setProvider({ signer: await InMemorySigner.fromSecretKey(private_key) });
+Tezos.setProvider({ signer: await InMemorySigner.fromSecretKey(privateKey.privateKey) });
 
 
-    app.post('/auction', async (req, res) => {
-      try {
-        const { auction_id, edition, token_id } = await req.body;
-        console.log()
-        // const contract = await Tezos.wallet.at(contrato)
-        // const op = contract.methods.finalize_auction(auction_id, edition, token_id).send();
-        // res.status(200).json({ message: `Transação aceita ${(await op).opHash}` });
+app.post('/auction', async (req, res) => {
+  try {
+        const { auction_id, edition, token_id } = req.body
+        console.log(auction_id, edition, token_id)
+
+        const contract = await Tezos.wallet.at(contrato)
+        const op = await contract.methods.finalize_auction(auction_id, edition, token_id).send();
+        res.status(200).json({ message: `Transação aceita  ${op.opHash}` });
           } catch (error) {
             console.error('Error', error)
             res.status(500).json({ error: 'An error occurred'})
