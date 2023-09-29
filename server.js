9aa31de1debfcf819d7ba5f7a6d2ab7fcf7b0629
BorderlessDev <borderlesspc1@gmail.com>
2023-09-29 13:00:26 -0300

    dinamico

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ import { TezosToolkit } from '@taquito/taquito';
 // Configurações
 const app = express();
 const port = process.env.PORT || 3000; // Use a porta definida no ambiente ou 3000
+app.use(express.json())
 
 const private_key = "edskS55SstJHyYXDJY2qZ8TT1s35ZqGtYqVPPyKiu5rkUhP6nfkWfWiFNhaDjcnY1uwYzho4yZAMAygPwQuawutZDtRrrcgWUZ"
 const contrato = "KT1EFLadgpu6EjSh4qrQP1BsxGyjP3cHh6cu"
@@ -13,9 +14,11 @@ Tezos.setProvider({ signer: await InMemorySigner.fromSecretKey(private_key) });
 
 app.get('/auction', async (req, res) => {
   try {
+    const { auction_id, edition, token_id } = req.body;
+    console.log(auction_id, edition, token_id)
     const contract = await Tezos.wallet.at(contrato)
-    const op = contract.methods.finalize_auction(1, 5, 1).send();
-    res.status(200).json({ message: `Transação aceita ${op}` });
+    const op = contract.methods.finalize_auction(auction_id, edition, token_id).send();
+    res.status(200).json({ message: `Transação aceita ${(await op).opHash}` });
       } catch (error) {
         console.error('Error', error)
         res.status(500).json({ error: 'An error occurred'})
