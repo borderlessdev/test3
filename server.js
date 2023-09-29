ee0e5e77ebf5500bfe95e74700fea5a4610da094
BorderlessDev <borderlesspc1@gmail.com>
2023-09-29 12:41:24 -0300

    função finalize - completa

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ app.get('/auction', async (req, res) => {
     app.post('/auction', async (req, res) => {
       try {
         const contract = await Tezos.wallet.at(contrato)
-        const op = contract.methods.finalize_auction(1, 5, 1).send();
+        const op = contract.methods.finalize_auction(2, 5, 2).send();
         res.status(200).json({ message: `Transação aceita ${op}` });
           } catch (error) {
             console.error('Error', error)
